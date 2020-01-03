04bef7c03f4594f33fad52a54fe3c1561d82ff5f
mihau <m.matusik@outlook.com>
2020-01-04 00:16:19 +0100

    Added Function Model;

---
+++ Models/FunctionModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityManager.Models
+{
+    public class FunctionModel
+    {
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            set 
+            { 
+                Name = value.Split('.')[1];
+                _type = value; 
+            }
+        }
+        public string Name { get; private set; }
+
+        public FunctionModel(string functionType)
+        {
+            this.Type = functionType;
+        }
+
+        public string ToVba()
+        {
+            return $"Public Function new{ Name }() As AllInterfaces.FactorableInterface: Set { Name } = New { Type }: End Function";
+        }
+
+    }
+}
