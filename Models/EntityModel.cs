06197db692e81c83225e1c1b8bf6674e6756477a
mihau <m.matusik@outlook.com>
2020-01-03 23:28:15 +0100

    Added entity model;

---
+++ Models/EntityModel.cs
@@ -0,0 +1,28 @@
+﻿namespace EntityManager.Models
+{
+    public class EntityModel
+    {
+        public string ShortName { get; set; }
+        public string LongName
+        {
+            get
+            {
+                return $"{ ShortName }Entity";
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return $"AllEntities.{ LongName }";
+            }
+        }
+        public string Repository
+        {
+            get
+            {
+                return $"{ ShortName }Repository";
+            }
+        }
+    }
+}
