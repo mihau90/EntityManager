561d3f982b45692d10f09f72d3b16f6edc908e0a
mihau <m.matusik@outlook.com>
2020-01-03 23:32:58 +0100

    Added string extensions;

---
+++ StringExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+
+namespace EntityManager
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": return "";
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+
+        public static string FirstCharToLower(this string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": return "";
+                default: return input.First().ToString().ToLower() + input.Substring(1);
+            }
+        }
+
+        public static string DropSuffix(this string input, string suffix)
+        {
+            var lcaseInput = input.ToLower();
+            var lcaseSuffix = suffix.ToLower();
+
+            if (lcaseInput.EndsWith(lcaseSuffix))
+            {
+                return input.Remove(lcaseInput.Length - lcaseSuffix.Length);
+            }
+            return input;
+        }
+
+        public static string ToSnakeCase(this string input)
+        {
+            return string.Concat(input.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+
+        public static string SnakeCaseToCamelCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            if (!input.Contains("_"))
+            {
+                return input;
+            }
+
+            string[] words = input.Split("_", StringSplitOptions.RemoveEmptyEntries);
+            string result = string.Empty;
+
+            foreach (string word in words)
+            {
+                result += word.Substring(0, 1).ToUpper() + word.Substring(1);
+            }
+
+            return result;
+        }
+    }
+}
