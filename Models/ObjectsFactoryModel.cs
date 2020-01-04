06830d59f6e405e3ce9a2226b190c47c4542969b
mihau <m.matusik@outlook.com>
2020-01-04 22:06:12 +0100

    removed ToVba() method;

--- Models/ObjectsFactoryModel.cs
+++ Models/ObjectsFactoryModel.cs
@@ -18,28 +18,5 @@ namespace EntityManager.Models
             Functions.Add(new FunctionModel(functionType));
             return this;
         }
-
-        public string ToVba()
-        {
-            var maxFunctionNameLenght = GetMaxFunctionNameLenght();
-            var maxFunctionDeclarationLenght = GetMaxFunctionDeclarationLenght();
-
-            var codeBlock = new List<string>() 
-            {
-                $"Attribute VB_Name = \"{ VbName }\"",
-                Description,
-                string.Empty,
-                "Option Explicit",
-                string.Empty,
-            };
-
-            var sortedList = Functions.OrderBy(x => x.Name).ToList();
-            var unduplicatedList = sortedList.GroupBy(x => x.Name).Select(x => x.First()).ToList();
-            var vbaFunctionsList = sortedList.Select(f => f.ToVba(maxFunctionNameLenght, maxFunctionDeclarationLenght)).ToList();
-
-            codeBlock.Add(string.Join(Environment.NewLine, vbaFunctionsList));
-
-            return string.Join(Environment.NewLine, codeBlock);
-        }
     }
 }
