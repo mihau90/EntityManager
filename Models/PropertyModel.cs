34dbefec5ce7266c2f56b34bab3da90d61730747
mihau <m.matusik@outlook.com>
2020-01-04 12:07:57 +0100

    Added setting default values for property and column;

--- Models/PropertyModel.cs
+++ Models/PropertyModel.cs
@@ -8,12 +8,11 @@ namespace EntityManager.Models
         private string _propertyName = string.Empty;
         private string _propertyType = string.Empty;
         private string _description = string.Empty;
-        private string _defaultValue = string.Empty;
+        private string _propertyDefaultValue = string.Empty;
         private string _columnName = string.Empty;
         private string _columnType = string.Empty;
         private int _charLimit = 0;
         private RelationType _relation = RelationType.None;
-
 
         public string PropertyName
         {
@@ -42,13 +41,13 @@ namespace EntityManager.Models
                 NotifyOfPropertyChange(() => Description);
             }
         }
-        public string DefaultValue
+        public string PropertyDefaultValue
         {
-            get { return _defaultValue; }
+            get { return _propertyDefaultValue; }
             set
             {
-                _defaultValue = value;
-                NotifyOfPropertyChange(() => DefaultValue);
+                _propertyDefaultValue = value;
+                NotifyOfPropertyChange(() => PropertyDefaultValue);
             }
         }
         public string ColumnName
@@ -87,13 +86,9 @@ namespace EntityManager.Models
                 NotifyOfPropertyChange(() => Relation);
             }
         }
-
-
         public bool IsGet { get; set; } = true;
         public bool IsSet { get; set; }
         public bool IsOptional { get; set; }
-
-
         public string ColumnDefinition
         {
             get
@@ -116,10 +111,6 @@ namespace EntityManager.Models
         {
             get
             {
-                if (string.IsNullOrEmpty(PropertyType))
-                {
-                    return false;
-                }
                 return PropertyType.EndsWith("[]");
             }
         }
@@ -127,12 +118,93 @@ namespace EntityManager.Models
         {
             get
             {
-                if (string.IsNullOrEmpty(PropertyType))
-                {
-                    return false;
-                }
                 return PropertyType.ToLower().StartsWith("allentities");
             }
+        }
+
+        public PropertyModel SetPropertyDefaultValue()
+        {
+            if (string.IsNullOrEmpty(PropertyType))
+            {
+                return this;
+            }
+
+            if (IsCollection)
+            {
+                PropertyDefaultValue = "New VBA.Collection";
+                return this;
+            }
+
+            if (IsAllAddInType)
+            {
+                PropertyDefaultValue = "Nothing";
+                return this;
+            }
+
+            switch (PropertyType)
+            {
+                case "Integer":
+                case "Long":
+                    PropertyDefaultValue = "0";
+                    break;
+
+                case "Double":
+                    PropertyDefaultValue = "0#";
+                    break;
+
+                case "Date":
+                    PropertyDefaultValue = "Null";
+                    break;
+
+                case "String":
+                    PropertyDefaultValue = "vbNullString";
+                    break;
+            }
+
+            return this;
+        }
+
+        public PropertyModel SetDefaultColumn()
+        {
+            ColumnName = PropertyName.ToSnakeCase();
+
+            if (string.IsNullOrEmpty(PropertyType))
+            {
+                return this;
+            }
+
+            if (IsAllAddInType || PropertyName == "id")
+            {
+                ColumnType = "CHAR";
+                CharLimit = 38;
+                return this;
+            }
+
+            switch (PropertyType.ToLower())
+            {
+                case "long":
+                case "integer":
+                    ColumnType = "INT";
+                    CharLimit = 0;
+                    break;
+
+                case "date":
+                    ColumnType = "DATETIME";
+                    CharLimit = 0;
+                    break;
+
+                case "double":
+                    ColumnType = "DOUBLE";
+                    CharLimit = 0;
+                    break;
+
+                case "string":
+                    ColumnType = "VARCHAR";
+                    CharLimit = 50;
+                    break;
+            }
+
+            return this;
         }
     }
 }
