ca83e60b349424dc67909d0194ae5b23d7f6dce1
mihau <m.matusik@outlook.com>
2020-01-04 13:06:34 +0100

    Config Manager added;

---
+++ ConfigManager.cs
@@ -0,0 +1,40 @@
+﻿using EntityManager.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace EntityManager
+{
+    public class ConfigManager
+    {
+        public ConfigModel Config { get; private set; }
+        public string ConfigFilePath { get; private set; }
+
+        public ConfigManager(string configFilePath)
+        {
+            this.ConfigFilePath = configFilePath;
+            Config = Load();
+        }
+        
+        public ConfigManager Save()
+        {
+            string output = JsonConvert.SerializeObject(Config, Formatting.Indented);
+            File.WriteAllText(ConfigFilePath, output);
+            return this;
+        }
+        private ConfigModel Load()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                return new ConfigModel();
+            }
+
+            var fileContent = File.ReadAllText(ConfigFilePath);
+            ConfigModel model = JsonConvert.DeserializeObject<ConfigModel>(fileContent);
+            
+            if (model == null)
+                model = new ConfigModel();
+
+            return model;
+        }
+    }
+}
