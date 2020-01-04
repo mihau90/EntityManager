c637dcfe18f727b145ba1b4dc0698a02ffe1f892
mihau <m.matusik@outlook.com>
2020-01-04 18:34:05 +0100

    added sql writer;

---
+++ Writers/SqlWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using EntityManager.CodeGenerators;
+using EntityManager.ViewModels;
+
+namespace EntityManager.Writers
+{
+    public class SqlWriter : IWriter
+    {
+        public string SqlFilePath { get; private set; }
+
+        public SqlWriter(string sqlFilePath)
+        {
+            this.SqlFilePath = sqlFilePath;
+        }
+
+        public void Write(ShellViewModel model)
+        {
+            string fileContents;
+            using (StreamReader reader = new StreamReader(SqlFilePath))
+            {
+                fileContents = reader.ReadToEnd();
+                reader.Close();
+            }
+
+            var sqlCodeGenerator = new SqlCodeGenerator();
+            var sqlCode = sqlCodeGenerator.Generate(model);
+
+            var tableDefinitionPattern = $@"CREATE TABLE { model.TableName } *\([\s\S]*?\);";
+            var regex = new Regex(tableDefinitionPattern);
+            var tableExists = regex.IsMatch(fileContents);
+
+            if (tableExists)
+            {
+                fileContents = regex.Replace(fileContents, sqlCode);
+            }
+            else
+            {
+                fileContents += sqlCode;
+            }
+
+            // remove blank lines
+            fileContents = Regex.Replace(fileContents, @"^\s*$[\r\n]*", string.Empty, RegexOptions.Multiline);
+            fileContents = Regex.Replace(fileContents, @"\);.*", $");{ Environment.NewLine }", RegexOptions.Multiline);
+
+            File.WriteAllText(SqlFilePath, fileContents);
+        }
+    }
+}
